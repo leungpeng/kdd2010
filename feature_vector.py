2a35c2b538afbee15a17713a0b7b71edb9d5a49d
vincentcheng62 <vincentcheng62@gmail.com>
2016-11-23 12:20:34 +0800

    Add different classifier methods, add time breakdown, add RAM usage, add detail output logging

--- feature_vector.py
+++ feature_vector.py
@@ -2,9 +2,17 @@
 # Usage: python project.py algebra_2005_2006
 import sys
 import random
-from project import load_data, show_data, rmse, process_step_name
+import gc
+import time
+from project import write_file, load_data, show_data, rmse, process_step_name
 from sklearn import svm
 from sklearn import linear_model
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+import os
+import psutil
 
 def get_feature_vector(element_set, value_set, w=1.0):
     result = [0.0] * len(element_set)
@@ -13,27 +21,39 @@ def get_feature_vector(element_set, value_set, w=1.0):
             result[element_set.index(val)] = w
     return result
 
-def get_feature_vectors(dataset, N, studentId_list, section_list, unit_list, problem_name_list, step_name_list, kc_list):
+def get_feature_vectors(dataset, N, studentId_list, unit_list, section_list, problem_name_list, step_name_list, kc_list):
     rows = []
     for i in range(1,N):
-        student_id_feature = get_feature_vector(studentId_list,[dataset[i][1]],100)
+        student_id_feature = get_feature_vector(studentId_list,[dataset[i][1]],10)
+        studentId_size = len(student_id_feature)
 
-        section, unit = dataset[i][2].split(", ")
+        unit, section = dataset[i][2].split(", ")
 
-        section_feature = get_feature_vector(section_list,[section], 50)
-        unit_feature = get_feature_vector(unit_list,[unit],25)
+        unit_feature = get_feature_vector(unit_list,[unit],1)
+        unit_size = len(unit_feature)
+        section_feature = get_feature_vector(section_list,[section], 1)
+        section_size = len(section_feature)
 
-        problem_name_feature = get_feature_vector(problem_name_list,[dataset[i][3]])
-        problem_view_feature = [float(dataset[i][4])]
+
+        #problem_name_feature = get_feature_vector(problem_name_list,[dataset[i][3]])
+        #problem_name_size = len(problem_name_feature)
+
+        problem_view_feature = [float(dataset[i][4])/(float(dataset[i][4])+1)]
+        problem_view_size = len(problem_view_feature)
 
         p_step = process_step_name(dataset[i][5])
-        step_name_feature = [ p_step.count('+'),p_step.count('-'),p_step.count('*'),p_step.count('/'),p_step.count('='),p_step.count('{var}')+p_step.count('{d}') ]
+        step_name_feature = [ p_step.count('+'),p_step.count('-'),p_step.count('*'),p_step.count('/'),p_step.count('{var}'),p_step.count('{d}') ]
+        #step_name_feature = [float(x)/sum(step_name_feature) for x in step_name_feature]
+        step_name_size = len(step_name_feature)  
+
         #print step_name_feature
-        kc_feature = get_feature_vector(kc_list, dataset[i][len(dataset[i])-2].split("~~") )
-        o = dataset[i][len(dataset[i])-1].split("~~")
+        kc_feature = get_feature_vector(kc_list, dataset[i][len(dataset[i])-2].split("~~"),1 )
+        kc_feature_size = len(kc_feature)        
+        #o = dataset[i][len(dataset[i])-1].split("~~")
 
         #print problem_hierarchy_feature
-        rows.append(student_id_feature + section_feature + unit_feature + problem_view_feature + step_name_feature + kc_feature)
+        rows.append(student_id_feature +  unit_feature + section_feature +problem_view_feature + step_name_feature + kc_feature)
+    print "feature vector composition: ", studentId_size, unit_size, section_size,problem_view_size, step_name_size, kc_feature_size
     return rows
 
 def process_data(training_data, testing_data):
@@ -46,10 +66,11 @@ def process_data(training_data, testing_data):
     is_first_correct_list = []
     kc_list = []
     testing_rows = []
-    N = 50000 #len(training_data)
+    N = 50000#len(training_data)
+    print "Num Of Lines to train: ", N
     for i in range(1,N):
         studentId = training_data[i][1]
-        section, unit = training_data[i][2].split(", ")
+        unit, section = training_data[i][2].split(", ")
         problem_name = training_data[i][3]
         step_name = process_step_name(training_data[i][5])
 
@@ -60,10 +81,10 @@ def process_data(training_data, testing_data):
 
         if studentId not in studentId_list:
             studentId_list.append(studentId)
-        if section not in section_list:
-            section_list.append(section)
         if unit not in unit_list:
             unit_list.append(unit)
+        if section not in section_list:
+            section_list.append(section)            
         if problem_name not in problem_name_list:
             problem_name_list.append(problem_name)
         if step_name not in step_name_list:
@@ -72,29 +93,69 @@ def process_data(training_data, testing_data):
             if kc not in kc_list:
                 kc_list.append(kc)
 
-    print len(studentId_list), len(section_list), len(unit_list), len(problem_name_list), len(step_name_list), len(kc_list)
+    print "#of unique item in each categories: ",len(studentId_list), len(unit_list), len(section_list), len(problem_name_list), len(step_name_list), len(kc_list)
     # Create matrix...
-    training_data_rows = get_feature_vectors(training_data, N, studentId_list, section_list, unit_list, problem_name_list, step_name_list, kc_list)
+    training_data_rows = get_feature_vectors(training_data, N, studentId_list, unit_list, section_list, problem_name_list, step_name_list, kc_list)
 
-    testing_data_rows = get_feature_vectors(testing_data, len(testing_data), studentId_list, section_list, unit_list, problem_name_list, step_name_list, kc_list)
+    testing_data_rows = get_feature_vectors(testing_data, len(testing_data), studentId_list, unit_list, section_list, problem_name_list, step_name_list, kc_list)
     return training_data_rows, is_first_correct_list, testing_data_rows
 
 
 def main(arg):
     dataset = arg[1] #'algebra_2005_2006'
+    start = time.time()
     training_data, testing_data, testing_result_data = load_data(dataset)
+    end = time.time()
+    print "Time to load data", end-start, " sec"
 
+    start = time.time()
     rows, is_first_correct_list, testing_rows = process_data(training_data, testing_data)
+    end = time.time()
+    print "Time to process data", end-start , " sec"   
     print len(rows),len(is_first_correct_list),len(testing_rows),len(rows[0])
 
-    #clf = linear_model.SGDClassifier(n_jobs=2,n_iter=10)
+    #print rows[:200]
+    #print testing_rows[:200]
 
-    clf = svm.SVC(gamma=0.001, C=100., verbose=True)
+    del training_data
+    del testing_data
+    gc.collect()
+
+    process = psutil.Process(os.getpid())
+    print "RAM usage (MB):", process.memory_info().rss/1024/1024
+
+    #write_file("preprocessed.txt", rows)
+
+    start = time.time()
+    #clf = linear_model.SGDClassifier(n_jobs=-1,n_iter=500)
+    #clf = linear_model.LogisticRegressionCV(n_jobs=-1, verbose=True)
+    clf = KNeighborsClassifier(n_jobs=-1, weights='distance', n_neighbors=5)
+    #clf = GaussianNB()
+    #clf = RandomForestClassifier(n_estimators=20,n_jobs=-1, verbose=True)
+    #clf = svm.SVC(verbose=True, cache_size=5000, kernel='linear', C=0.5)
+    #clf = tree.DecisionTreeClassifier()
+
     clf.fit(rows, is_first_correct_list)
     print clf
+
+    end = time.time()
+    print "Time to train classifier", end-start, " sec"
+
+    process = psutil.Process(os.getpid())
+    print "RAM usage (MB):", process.memory_info().rss/1024/1024
+
+    start = time.time()
     predict_result = clf.predict(rows)
+    end = time.time()
+    print "Time to do prediction of self-test", end-start, " sec"
+
+    #print "Mean accuracy" , clf.score(rows, is_first_correct_list)
+    print "first 50 items of predict: ", predict_result[:50]
+    print "first 50 items of GT: ", is_first_correct_list[:50]
     predict_result = [ float(i) for i in predict_result]
     training_error = rmse(predict_result, [ float(i) for i in is_first_correct_list])
+    print "rmse of first 10 items ", rmse([ float(i) for i in predict_result[:10]], [ float(i) for i in is_first_correct_list[:10]])
+
 
     predict_result = clf.predict(testing_rows)
     predict_result = [ float(i) for i in predict_result]
@@ -105,3 +166,4 @@ def main(arg):
 
 if __name__ == "__main__":
     main(sys.argv)
+
