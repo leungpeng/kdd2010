00ebdd77be82ac58d5651fb6a78598cb37962bda
vincentcheng62 <vincentcheng62@gmail.com>
2016-12-13 10:27:38 +0800

    tune the parameters

--- all_in_one.py
+++ all_in_one.py
@@ -5,38 +5,37 @@ from sklearn import svm, linear_model, tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.ensemble import RandomForestClassifier
-import os, numpy, sys, random
+import os, numpy, sys, random, time
 from feature_vector import process_data
 from cf import training, predict_from_matrix
 
 def main(arg):
     dataset = arg[1] #'algebra_2005_2006'
-    NumOfLineToTrain = 50000
     training_data, testing_data, testing_result_data = load_data(dataset)
+    NumOfLineToTrain = 50000 #len(training_data)
 
     # mode0: normal, 1: normal+condensed, 2: only condensed
     Feature_vector_mode = 0
     rows, CFA_list, testing_rows, test_CFA = process_data(training_data,\
      testing_data, testing_result_data, NumOfLineToTrain, False, Feature_vector_mode)
-    print len(rows),len(CFA_list),len(testing_rows),len(rows[0])
+    print 'Training rows:', len(rows),'Testing rows:', len(testing_rows), \
+    '# of features:', len(rows[0])
 
 
-    clf=[]
-    y_pred_list=[]
-    name_list=['KNN', 'RandomForest', 'LinearSVM', 'NaiveBayes', \
+    clf=[]; y_pred_list=[]; M=3 #number of classifier
+    name_list=['KNN', 'RandomForest', 'LinearSVM', \
     'Collabrative filtering']
     #y_pred_list.append([random.randint(0,1) for i in testing_rows])
 
     ##############################################################
-    M=4 # number of classifier
 
     #clf = KNeighborsClassifier(n_jobs=-1, weights='distance', n_neighbors=5, metric='pyfunc', func=myknndist)
-    clf.append(KNeighborsClassifier(n_jobs=-1, weights='distance', n_neighbors=5, p=2))
-    clf.append(RandomForestClassifier(n_estimators=50,n_jobs=-1, verbose=False))
-    clf.append(svm.LinearSVC(verbose=False, C=1.0))
+    clf.append(KNeighborsClassifier(n_jobs=-1, weights='distance', n_neighbors=10, p=2))
+    clf.append(RandomForestClassifier(n_estimators=100,n_jobs=-1, verbose=False))
+    clf.append(svm.LinearSVC(verbose=False, C=4.0))
     #clf = tree.DecisionTreeClassifier()
 
-    clf.append(GaussianNB())
+    #clf.append(GaussianNB())
     #clf = MultinomialNB(alpha=1.0)
     #clf.append(BernoulliNB(alpha=2.0, binarize=1.0))
 
@@ -45,17 +44,23 @@ def main(arg):
 
     #############################################################
 
-
     #Train and do prediction for each method
     for i in range(M):
+        start = time.time()
         print 'Training', name_list[i], '...'
         clf[i].fit(rows, CFA_list)
         print 'Predicting', name_list[i], '...'
         y_pred_list.append(clf[i].predict(testing_rows))
+        end = time.time()
+        print "Time elapse: ", end-start, " sec"
 
-    matrix, students, problems, testing_sample = training(training_data[:NumOfLineToTrain])
+    start = time.time()
+    learnrate = 0.01; regular = 0.02; numofstep = 100
+    matrix, students, problems, testing_sample = training(training_data[:NumOfLineToTrain], learnrate, regular, numofstep)
     y_pred_list.append(predict_from_matrix(matrix, students, problems,\
         [ (data[1].upper(), data[3].upper()) for data in testing_data[1:]]))
+    end = time.time()
+    print "Time elapse: ", end-start, " sec"
 
     for i in range(len(name_list)):
         print name_list[i], ' rmse= ', rmse(y_pred_list[i], test_CFA)
